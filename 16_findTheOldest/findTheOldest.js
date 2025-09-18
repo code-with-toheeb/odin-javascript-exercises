228dfb214dac2e21b764e4202e0eb97d38d6e3b4
Olanrewaju Toheeb Olawale <toheebolawale019@gmail.com>
2025-09-18 10:35:58 +0100

    done

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+
+  return array.reduce((acc, person) => {
+
+    let year = new Date()
+    let age = person.yearOfDeath - person.yearOfBirth;
+
+    let accAge = acc.yearOfDeath - acc.yearOfBirth;
+
+    if (person.yearOfDeath == undefined)
+    {
+      age = year.getFullYear() - person.yearOfBirth
+    }
+
+    if (acc.yearOfDeath == undefined)
+    {
+      accAge = year.getFullYear() - acc.yearOfBirth;
+    }
+    return age > accAge ? person : acc;
+
+  })
+
+
+
 
 };
 
